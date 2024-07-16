b0abeecf2784c2c396e3027ecce32a4931c36a75
goutembergpessoa <goutemberg.silva@foxbit.com.br>
2024-07-16 15:33:52 -0300

    Insercao de icones e ajustes na telas

--- home/models.py
+++ home/models.py
@@ -1,38 +1,115 @@
 from django.db import models
+from django.core.validators import RegexValidator, EmailValidator
 
-class cadastroEmpresa(models.Model):
-    nomeEmp = models.CharField(max_length=260)
-    razaoSocialEmp = models.CharField(max_length=260)
-    cnpjEmp = models.CharField(max_length=260)
-    inscEstEmp = models.CharField(max_length=260)
-    cepEmp = models.CharField(max_length=9)
-    lougradouroEmp = models.CharField(max_length=260)
-    numeroEmp = models.CharField(max_length=5)
-    bairroEmp = models.CharField(max_length=260)
-    cidadeEmp = models.CharField(max_length=100)
-    estadoEmp = models.CharField(max_length=4)
-    telefoneEmp = models.CharField(max_length=16)
-    celularEmp = models.CharField(max_length=16)
-    pessoaContEmp = models.CharField(max_length=50)
-    emailEmp = models.CharField(max_length=50)
-    isentoTribEmp = models.CharField(max_length=5)
-    taxaAdmEmp = models.CharField(max_length=5)
+class CadastroEmpresa(models.Model):
+    nome = models.CharField(max_length=255)
+    razao_social = models.CharField(max_length=255)
+    cnpj = models.CharField(
+        max_length=14, 
+        primary_key=True,
+        validators=[
+            RegexValidator(regex='^\d{14}$', message='CNPJ deve conter 14 dígitos numéricos')
+        ]
+    )
+    inscricao_estadual = models.CharField(max_length=255)
+    cep = models.CharField(max_length=9, validators=[
+        RegexValidator(regex='^\d{5}-?\d{3}$', message='CEP deve estar no formato XXXXX-XXX')
+    ])
+    logradouro = models.CharField(max_length=255)
+    numero = models.CharField(max_length=5)
+    bairro = models.CharField(max_length=255)
+    cidade = models.CharField(max_length=100)
+    estado = models.CharField(max_length=2)
+    telefone = models.CharField(max_length=16, validators=[
+        RegexValidator(regex='^\(?\d{2}\)?[\s-]?\d{4,5}-?\d{4}$', message='Telefone deve estar no formato (XX) XXXX-XXXX ou (XX) XXXXX-XXXX')
+    ])
+    celular = models.CharField(max_length=16, validators=[
+        RegexValidator(regex='^\(?\d{2}\)?[\s-]?\d{4,5}-?\d{4}$', message='Celular deve estar no formato (XX) XXXXX-XXXX')
+    ])
+    pessoa_contato = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50, validators=[EmailValidator(message='Email inválido')])
+    isento_tributacao = models.BooleanField(default=False)
+    taxa_administrativa = models.DecimalField(max_digits=6, decimal_places=2)
 
-class valorPlantao(models.Model):
-    valorPlantao_12 = models.CharField(max_length=10)
-    valorPlantaoHora = models.CharField(max_length=10)
-    valorPlantaoSemana = models.CharField(max_length=10)
-    valorPlantaoSabDom = models.CharField(max_length=10)
+class ValorPlantao(models.Model):
+    valor_12h = models.DecimalField(max_digits=10, decimal_places=2)
+    valor_por_hora = models.DecimalField(max_digits=10, decimal_places=2)
+    valor_semana = models.DecimalField(max_digits=10, decimal_places=2)
+    valor_fim_semana = models.DecimalField(max_digits=10, decimal_places=2)
 
+class InformacoesBancarias(models.Model):
+    banco = models.CharField(max_length=50)
+    agencia = models.CharField(max_length=10, validators=[
+        RegexValidator(regex='^\d{4,10}$', message='Agência deve conter de 4 a 10 dígitos numéricos')
+    ])
+    conta = models.CharField(max_length=10, validators=[
+        RegexValidator(regex='^\d{5,10}$', message='Conta deve conter de 5 a 10 dígitos numéricos')
+    ])
+    titular_conta = models.CharField(max_length=255)
 
-class cadastroMedico(models.Model):
-   nome = models.CharField(max_length=260)
-   especialidade = models.CharField(max_length=100)
-   crm = models.CharField(max_length=10)
-   telefone = models.CharField(max_length=12)
+    def __str__(self):
+        return f'{self.titular_conta} - {self.banco}'
 
-class plantao(models.Model):
-    data = models.DateField()
-    medico = models.CharField(max_length=260)
-    hora = models.TimeField()
-    valor = models.CharField(max_length=12)
+class CadastroMedico(models.Model):
+    nome = models.CharField(max_length=255)
+    cpf = models.CharField(max_length=14, primary_key=True, validators=[
+        RegexValidator(regex='^\d{11}$', message='CPF deve conter 11 dígitos numéricos')
+    ])
+    especialidade = models.CharField(max_length=100)
+    crm = models.CharField(max_length=20)
+    celular = models.CharField(max_length=16, validators=[
+        RegexValidator(regex='^\(?\d{2}\)?[\s-]?\d{4,5}-?\d{4}$', message='Celular deve estar no formato (XX) XXXXX-XXXX')
+    ])
+    logradouro = models.CharField(max_length=255)
+    numero = models.CharField(max_length=5)
+    complemento = models.CharField(max_length=255, blank=True, null=True)
+    bairro = models.CharField(max_length=255)
+    cidade = models.CharField(max_length=255)
+    estado = models.CharField(max_length=2)
+    telefone1 = models.CharField(max_length=16, validators=[
+        RegexValidator(regex='^\(?\d{2}\)?[\s-]?\d{4,5}-?\d{4}$', message='Telefone deve estar no formato (XX) XXXX-XXXX ou (XX) XXXXX-XXXX')
+    ])
+    telefone2 = models.CharField(max_length=16, blank=True, null=True, validators=[
+        RegexValidator(regex='^\(?\d{2}\)?[\s-]?\d{4,5}-?\d{4}$', message='Telefone deve estar no formato (XX) XXXX-XXXX ou (XX) XXXXX-XXXX')
+    ])
+    email = models.EmailField(max_length=100, validators=[EmailValidator(message='Email inválido')])
+    informacoes_bancarias = models.OneToOneField(InformacoesBancarias, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nome
+
+class Atendente(models.Model):
+    nome = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.nome
+
+class InformacoesClinica(models.Model):
+    nome = models.CharField(max_length=255)
+    atendentes = models.ManyToManyField(Atendente, related_name='clinicas')
+    telefone = models.CharField(max_length=16, validators=[
+        RegexValidator(regex='^\(?\d{2}\)?[\s-]?\d{4,5}-?\d{4}$', message='Telefone deve estar no formato (XX) XXXX-XXXX ou (XX) XXXXX-XXXX')
+    ])
+
+    def __str__(self):
+        return self.nome
+
+class Plantao(models.Model):
+    data_inicio = models.DateField()
+    hora_inicio = models.TimeField()
+    data_termino = models.DateField()
+    hora_termino = models.TimeField()
+    medico_responsavel = models.ForeignKey(CadastroMedico, on_delete=models.CASCADE)
+    especialidade = models.CharField(max_length=255)
+    tipo_plantao = models.CharField(max_length=20)
+    quantidade_horas = models.DecimalField(max_digits=5, decimal_places=2)
+    valor = models.DecimalField(max_digits=10, decimal_places=2)
+    status = models.CharField(max_length=12)
+    contato_emergencia = models.CharField(max_length=255)
+    equipamentos_necessarios = models.TextField()
+    cargos_auxiliares = models.TextField()
+    substituto = models.CharField(max_length=255)
+    observacoes = models.TextField()
+
+    def __str__(self):
+        return f'Plantão {self.tipo_plantao} de {self.medico_responsavel}'        
