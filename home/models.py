f6742be6371634f6a14779e70b20dc2d09577c1c
goutembergpessoa <goutemberg.silva@foxbit.com.br>
2024-07-16 21:52:54 -0300

    Finalizacao_tabelas

--- home/models.py
+++ home/models.py
@@ -1,4 +1,9 @@
 from django.db import models
+from django.utils import timezone
+from django.core.validators import RegexValidator, EmailValidator
+
+from django.db import models
+from django.utils import timezone
 from django.core.validators import RegexValidator, EmailValidator
 
 class CadastroEmpresa(models.Model):
@@ -23,13 +28,16 @@ class CadastroEmpresa(models.Model):
     telefone = models.CharField(max_length=16, validators=[
         RegexValidator(regex='^\(?\d{2}\)?[\s-]?\d{4,5}-?\d{4}$', message='Telefone deve estar no formato (XX) XXXX-XXXX ou (XX) XXXXX-XXXX')
     ])
-    celular = models.CharField(max_length=16, validators=[
+    celular = models.CharField(max_length=16, default='00000000000', validators=[
         RegexValidator(regex='^\(?\d{2}\)?[\s-]?\d{4,5}-?\d{4}$', message='Celular deve estar no formato (XX) XXXXX-XXXX')
-    ])
+    ])  # Adicionado valor padrão
     pessoa_contato = models.CharField(max_length=50)
     email = models.EmailField(max_length=50, validators=[EmailValidator(message='Email inválido')])
     isento_tributacao = models.BooleanField(default=False)
     taxa_administrativa = models.DecimalField(max_digits=6, decimal_places=2)
+    valor_imposto = models.CharField(max_length=5)
+
+
 
 class ValorPlantao(models.Model):
     valor_12h = models.DecimalField(max_digits=10, decimal_places=2)
@@ -95,10 +103,10 @@ class InformacoesClinica(models.Model):
         return self.nome
 
 class Plantao(models.Model):
-    data_inicio = models.DateField()
-    hora_inicio = models.TimeField()
-    data_termino = models.DateField()
-    hora_termino = models.TimeField()
+    data_inicio = models.DateField(default=timezone.now)
+    hora_inicio = models.TimeField(default=timezone.now)
+    data_termino = models.DateField(default=timezone.now)
+    hora_termino = models.TimeField(default=timezone.now)
     medico_responsavel = models.ForeignKey(CadastroMedico, on_delete=models.CASCADE)
     especialidade = models.CharField(max_length=255)
     tipo_plantao = models.CharField(max_length=20)
@@ -109,7 +117,8 @@ class Plantao(models.Model):
     equipamentos_necessarios = models.TextField()
     cargos_auxiliares = models.TextField()
     substituto = models.CharField(max_length=255)
-    observacoes = models.TextField()
+    observacoes = models.TextField(max_length=255)
 
     def __str__(self):
-        return f'Plantão {self.tipo_plantao} de {self.medico_responsavel}'        
+        return f'Plantão {self.tipo_plantao} de {self.medico_responsavel}'
+    
