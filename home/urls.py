614b8f0095f015e74c10030f779bd6e6f252f1e4
goutembergpessoa <goutemberg.silva@foxbit.com.br>
2024-07-18 22:44:07 -0300

    Insercao no banco nome e nome fantasia

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('cadastrarEmpresa/', views.cadastrarEmpresa, name='cadastrarEmpresa'),
+    path('cadastroEmpBanco/', views.cadastroEmpBanco, name='cadastroEmpBanco'),
+ 
 ]
--- home/views.py
+++ home/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
+from django.urls import reverse
+from .models import CadastroEmpresa
 
 def index(request):
+    cadastroEmpresa = CadastroEmpresa.objects.all().values()
     template = loader.get_template('index.html')
-    return HttpResponse(template.render())
+    context = {
+        'cadastroEmpresa':cadastroEmpresa
+    }
+    return HttpResponse(template.render(context,request))
 
+
+
+def cadastrarEmpresa(request):
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render({}, request))
+
+def cadastroEmpBanco(request):
+    CompanyName = request.POST['companyName']
+    CompanyLegalName = request.POST ['companyLegalName']
+    novoCadastroEmp = CadastroEmpresa(nome=CompanyName,razao_social=CompanyLegalName)
+    novoCadastroEmp.save()
+    return HttpResponseRedirect(reverse('index'))
