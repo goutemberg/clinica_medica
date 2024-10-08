f7c1928f0d7b16a23611f64cb71accd940118633
goutembergpessoa <goutemberg.silva@foxbit.com.br>
2024-10-08 20:30:30 -0300

    ALLOWED_HOSTS

--- plantao_pro/settings.py
+++ plantao_pro/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'django-insecure-&3d@0w2x0^@2juw_v478r0h_4rk6)iv_*o6r&hc0#eq!)th3&y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://clinica-medica-i66x.onrender.com','localhost', '127.0.0.1','clinica-medica-i66x.onrender.com']
 
 
 # Application definition
